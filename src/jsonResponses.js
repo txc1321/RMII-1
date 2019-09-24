e43b171d6b54180bb96f170d7a19d0a56c15429b
Timothy Carter (RIT Student) <txc1321@ad.rit.edu>
2019-09-24 13:42:24 -0400

    Updates to try and handle different get responses

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,54 +1,68 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, { 'Content-Type': 'application/json' });
-    response.write(JSON.stringify(object));
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondMeta = (request, response, status) => {
-    const headers = { 'Content-Type': 'application/json' };
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
 };
 
-const getUsers = (request, response) => {
-    const responseJSON = {
-        users,
-    };
-    respondJSON(request, response, 200, responseJSON);
+const getUsers = (request, response, body) => {
+  const responseJSON = {};
+
+  if (body.method === 'HEAD') {
+    if (body.url === '/getUsers') {
+      return respondMeta(request, response, 200);
+    }
+
+    return respondMeta(request, response, 404);
+  }
+
+  if (body.url === '/getUsers') {
+    console.dir("test");
+    responseJSON.users = users;
+    return respondJSON(request, response, 200, responseJSON);
+  }
+
+  responseJSON.id = 'notFound';
+  responseJSON.message = 'The page you are looking for was not found';
+  return respondJSON(request, response, 404, responseJSON);
 };
 
 const addUser = (request, response, body) => {
-    const responseJSON = {
-        message: 'Name and age are both required',
-    };
+  const responseJSON = {
+    message: 'Name and age are both required',
+  };
 
-    if(!body.name || !body.age){
-        responseJSON.id = 'missingParams';
-        return respondJSON(request, response, 400, responseJSON);
-    }
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
 
-    let responseCode = 201;
+  let responseCode = 201;
 
-    if(users[body.name]){
-        responseCode = 204;
-    }
-    else{
-        users[body.name] = {};
-    }
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+  }
 
-    users[body.name].name = body.name;
-    users[body.name].age = body.age;
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
 
-    if(responseCode === 201){
-        responseJSON.message = 'Created Successfully';
-        return  respondJSON(request, response, responseCode, responseJSON);
-    }
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
 
-    return respondMeta(request, response, responseCode);
+  return respondMeta(request, response, responseCode);
 };
 
 module.exports = {
-    getUsers,
-    addUser,
+  getUsers,
+  addUser,
 };
--- src/server.js
+++ src/server.js
@@ -2,63 +2,84 @@ const http = require('http');
 const url = require('url');
 const query = require('querystring');
 const htmlHandler = require('./htmlResponses');
-const jsonHandler = require('./responses');
+const jsonHandler = require('./jsonResponses');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// TODO:
+// - Send writes data to body and console, not redirect
+//   (getUsers, GET, empty and containing list; getUsers, HEAD; notReal, Get (404); notReal, HEAD)
+// - Add user with existing name: update body and fix error
+// - Direct call to any other url - notFound
+
 const handlePost = (request, response, parsedUrl) => {
-    if(parsedUrl.pathname === '/addUser'){
-        const res = response;
+  if (parsedUrl.pathname === '/addUser') {
+    const res = response;
 
-        const body = [];
+    const body = [];
 
-        request.on('error', (err) => {
-            console.dir(err);
-            res.statusCode = 400;
-            res.end();
-        });
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
 
-        request.on('end', () => {
-            const bodyString = Buffer.concat(body).toString();
-            const bodyParams = query.parse(bodyString);
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+      console.dir(bodyString);
 
-            jsonHandler.addUser(request, res, bodyParams);
-        });
-    }
+      jsonHandler.addUser(request, res, bodyParams);
+    });
+  }
 };
 
 const handleGet = (request, response, parsedUrl) => {
-    if(parsedUrl.pathname === '/style.css'){
+  if (parsedUrl.pathname === '/getUsers') {
+    const res = response;
+
+    const body = [];
+
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+      console.dir(bodyString);
+
+      jsonHandler.getUsers(request, res, bodyParams);
+    });
+  }
+  else if (parsedUrl.pathname === '/style.css') {
         htmlHandler.getCSS(request, response);
-    }
-    else if(parsedUrl.pathname === '/getUsers'){
-        jsonHandler.getUsers(request, response);
-    }
-    else{
-        htmlHandler.getIndex(request, response);
-    }
-};
-
-const handleHead = (request, response, parsedUrl) => {
-
+  }
+  else {
+    htmlHandler.getIndex(request, response);
+  }
 };
 
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
+  const parsedUrl = url.parse(request.url);
 
-    if(request.method === 'POST'){
-        handlePost(request, response, parsedUrl);
-    }
-    else if(request.method === 'GET'){
-        handleGet(request, response, parsedUrl);
-    }
-    else{
-        handleHead();
-    }
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else if (request.method === 'GET') {
+    handleGet(request, response, parsedUrl);
+  } else {
+    // notFound
+  }
 };
 
 http.createServer(onRequest).listen(port);
